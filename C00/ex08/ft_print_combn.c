853d656695240cfe0908cc3239e4b9113915629f
AlexVplle <alexisvapaille11@gmail.com>
2022-07-08 01:46:44 +0200

    C00

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -1,42 +1,53 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: avapaill <avapaill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/07 22:33:36 by avapaill          #+#    #+#             */
-/*   Updated: 2022/07/08 00:37:59 by avapaill         ###   ########.fr       */
+/*   Created: 2022/07/08 00:39:01 by avapaill          #+#    #+#             */
+/*   Updated: 2022/07/08 01:45:46 by avapaill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void	write_pattern(int n, char array_number[])
 {
-	write(1, &c, 1);
+	int	j;
+
+	j = 0;
+	while (j < n)
+	{
+		write(1, &array_number[j], 1);
+		j++;
+	}
+	write(1, " ", 1);
 }
 
-void	ft_putnbr(int nb)
+void	ft_print_combn(int n)
 {
-	int	unity_to_process;
-	int	next_to_process;
+	char	array_number[n];
+	int		i;
 
-	unity_to_process = nb % 10;
-	next_to_process = nb / 10;
-	if (!next_to_process)
+	i = 0;
+	while (i < n)
 	{
-		ft_putchar(nb + '0');
+		array_number[i] = i + '0';
+		i++;
 	}
-	else if (next_to_process < 0)
+	while (i >= 0)
 	{
-		ft_putchar('-');
-		ft_putnbr((-1) * next_to_process);
-		ft_putchar(-unity_to_process + '0');
+		while (array_number[i] < '9' - n + i + 1)
+		{
+			write_pattern(n, array_number);
+			array_number[i]++;
+		}
+		i--;
 	}
-	else
-	{
-		ft_putnbr(next_to_process);
-		ft_putnbr(unity_to_process);
-	}
+}
+
+int	main(void)
+{
+	ft_print_combn(3);
 }
