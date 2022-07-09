8eff9bccc34d81fc24ef065501c28e761a5c79b1
AlexVplle <alexisvapaille11@gmail.com>
2022-07-10 00:48:36 +0200

    finish C01

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: avapaill <avapaill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 22:33:36 by avapaill          #+#    #+#             */
-/*   Updated: 2022/07/08 00:37:59 by avapaill         ###   ########.fr       */
+/*   Updated: 2022/07/09 23:57:20 by avapaill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,25 @@ void	ft_putchar(char c)
 }
 
 void	ft_putnbr(int nb)
-{
-	int	unity_to_process;
-	int	next_to_process;
-
-	unity_to_process = nb % 10;
-	next_to_process = nb / 10;
-	if (!next_to_process)
+{	
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb >= 0 && nb < 10)
 	{
 		ft_putchar(nb + '0');
-	}
-	else if (next_to_process < 0)
+		return ;
+	}	
+	if (nb < 0)
 	{
 		ft_putchar('-');
-		ft_putnbr((-1) * next_to_process);
-		ft_putchar(-unity_to_process + '0');
+		ft_putnbr(-nb);
 	}
 	else
 	{
-		ft_putnbr(next_to_process);
-		ft_putnbr(unity_to_process);
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
 	}
 }
