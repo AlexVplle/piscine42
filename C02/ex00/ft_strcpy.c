42c996987d1bf7ff7f77822c3ba4b7479a9e853f
AlexVplle <alexisvapaille11@gmail.com>
2022-07-10 01:37:36 +0200

    start C02

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avapaill <avapaill@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/10 01:07:13 by avapaill          #+#    #+#             */
+/*   Updated: 2022/07/10 01:36:42 by avapaill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int	main(void)
+{
+	char	*src;
+	char	*dest;
+
+	src = "test";
+	dest = "          ";
+	ft_strcpy(dest, src);
+	printf("%s", dest);
+}
