e5364b975b40ffd9984e51966456190ec3969641
AlexVplle <alexisvapaille11@gmail.com>
2022-07-11 01:12:30 +0200

    bien avance

---
+++ C02/ex09/ft_strcapitalize.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avapaill <avapaill@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 00:43:03 by avapaill          #+#    #+#             */
+/*   Updated: 2022/07/11 01:12:10 by avapaill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+	int	capitalize;
+	char	an[];
+
+	i = 0
+	capitalize = 1;
+	an = "abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	while (str[i])
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= '0' && str[i] <= '9'))
+	}
+}
