f7f49e054f3386c6afd218f1e4df9c2e75c7d686
AlexVplle <alexisvapaille11@gmail.com>
2022-07-14 00:26:20 +0200

    finish C03 pour de vrai

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: avapaill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 18:15:29 by avapaill          #+#    #+#             */
-/*   Updated: 2022/07/13 17:29:05 by avapaill         ###   ########.fr       */
+/*   Updated: 2022/07/14 00:25:49 by avapaill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,21 @@ int	ft_strlen(char *str)
 char	*ft_strstr(char *str, char *to_find)
 {
 	int	count;
-	int	count2;
-	int	found;
+	int	length;
 
+	length = ft_strlen(to_find);
+	if (!length)
+		return (str);
 	count = 0;
-	while (count < ft_strlen(str))
+	while (*str)
 	{
-		found = 1;
-		count2 = 0;
-		while (count2 < ft_strlen(to_find))
-		{
-			if (to_find[count2] != str[count + count2])
-			{
-				found = 0;
-				break ;
-			}
-			count2++;
-		}
-		if (found)
-			return (&str[count]);
-		count++;
+		if (*str == to_find[count])
+			count++;
+		else
+			count = 0;
+		if (count == length)
+			return (str - count + 1);
+		str++;
 	}
 	return ((void *) 0);
 }
