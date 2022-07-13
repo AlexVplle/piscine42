847c8a1c134713eb9938d7c3d41a86630be80b68
Alexis VAPAILLE <avapaill@e3r4p14.clusters.42paris.fr>
2022-07-13 11:15:05 +0200

    finish C03

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,50 +6,9 @@
 /*   By: avapaill <avapaill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 23:17:15 by avapaill          #+#    #+#             */
-/*   Updated: 2022/07/13 00:50:37 by avapaill         ###   ########.fr       */
+/*   Updated: 2022/07/13 11:12:57 by avapaill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <bsd/string.h>
-#include <stdio.h>
-
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
-int	main(void)
-{
-	char s1a[10] = "Test1";
-	char s2a[] = "OK";
-	char s1b[10] = "Test1";
-	char s2b[] = "OK";
-	char s3a[10] = "Same";
-	char s4a[] = "Size";
-	char s3b[10] = "Same";
-	char s4b[] = "Size";
-	char s5a[20] = "Shorter";
-	char s6a[] = "ThanMyself";
-	char s5b[20] = "Shorter";
-	char s6b[] = "ThanMyself";
-	char s7a[20] = "Shorter";
-	char s8a[] = "ThanMyself";
-	char s7b[20] = "Shorter";
-	char s8b[] = "ThanMyself";
-
-	printf("%s\n", ft_strlcat(s1a, s2a, 6) == strlcat(s1b, s2b, 6) ? "Success" : "Fail");
-	printf("%s\n", strcmp(s1a, s1b) == 0 && strcmp(s2a, s2b) == 0 ? "Success" : "Fail");
-	printf("%s\n", ft_strlcat(s3a, s4a, 10) == strlcat(s3b, s4b, 10) ? "Success" : "Fail");
-	printf("%s\n", strcmp(s3a, s3b) == 0 && strcmp(s4a, s4b) == 0 ? "Success" : "Fail");
-	printf("%s\n", ft_strlcat(s5a, s6a, 4) == strlcat(s5b, s6b, 4) ? "Success" : "Fail");
-	printf("%s\n", strcmp(s5a, s5b) == 0 && strcmp(s6a, s6b) == 0 ? "Success" : "Fail");
-	printf("%s\n", ft_strlcat(s7a, s8a, 0) == strlcat(s7b, s8b, 0) ? "Success" : "Fail");
-	printf("%s\n", strcmp(s7a, s7b) == 0 && strcmp(s8a, s8b) == 0 ? "Success" : "Fail");
-	
-
-	printf("\n\nDetails:\n");
-	printf("%s:%s\n%s:%s\n", s1a, s1b, s2a, s2b);
-	printf("%s:%s\n%s:%s\n", s3a, s3b, s4a, s4b);
-	printf("%s:%s\n%s:%s\n", s5a, s5b, s6a, s6b);
-	printf("%s:%s\n%s:%s\n", s7a, s7b, s8a, s8b);
-
-}
 
 unsigned int	ft_strlen(char *str)
 {
@@ -69,11 +28,13 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	length = ft_strlen(dest);
 
 	i = 0;
-	while (length + i < size - 1 && src[i])
+	while (length + i + 1 < size && src[i])
 	{
 		dest[length + i] = src[i];
 		i++;
 	}
 	dest[length + i] = '\0';
-	return (length + i);
+	if (length <= size)
+		return (length + strlen(src));
+	return (size + strlen(src));
 }
