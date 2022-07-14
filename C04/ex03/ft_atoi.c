05dedaa990c04a8526957a5d31ad6e21fec04a64
Alexis VAPAILLE <avapaill@e3r11p3.clusters.42paris.fr>
2022-07-14 19:04:13 +0200

    first C04

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,46 +6,39 @@
 /*   By: avapaill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 17:55:21 by avapaill          #+#    #+#             */
-/*   Updated: 2022/07/14 12:06:14 by avapaill         ###   ########.fr       */
+/*   Updated: 2022/07/14 18:10:33 by avapaill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
 char	*remove_white_space(char *str)
 {
-	while(*str < 33)
+	while (*str == 32 || (*str >= 9 && *str <= 13))
 		str++;
 	return (str);
 }
 
 unsigned int	create_number(char *str, int number)
 {
-//	printf("%s", str);
 	if (*str >= '0' && *str <= '9')
-	{
-		
-		return create_number(number * 10 + *str - '0');
-	}
+		return (create_number(str +1, number * 10 + *str - '0'));
 	return (number);
 }
 
 int	ft_atoi(char *str)
 {
-	int	negative;
 	unsigned int	number;
+	int				negative;
+
+	negative = 0;
 	str = remove_white_space(str);
-	while(*str == '+' || *str == '-')
+	while (*str == '+' || *str == '-')
 	{
 		if (*str == '-')
 			negative++;
 		str++;
 	}
 	number = create_number(str, 0);
-	printf("%u", number);
-}
-
-int	main(void)
-{
-	ft_atoi("152");
+	if (negative % 2)
+		return (-1 * number);
+	return (number);
 }
