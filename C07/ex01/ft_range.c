9fe9381ea47b7d659b9aba593dff42018f698193
Alexis VAPAILLE <avapaill@e3r10p4.clusters.42paris.fr>
2022-07-19 19:34:32 +0200

    first

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -6,12 +6,11 @@
 /*   By: avapaill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 09:27:09 by avapaill          #+#    #+#             */
-/*   Updated: 2022/07/19 10:50:31 by avapaill         ###   ########.fr       */
+/*   Updated: 2022/07/19 18:50:22 by avapaill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
 
 int	*ft_range(int min, int max)
 {
