54cf3d387ac02e3def0f86d2fbddd072d1d20c5e
Alexis VAPAILLE <avapaill@e3r11p5.42.fr>
2022-07-20 19:34:58 +0200

    finish

---
+++ C08/ex00/ft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avapaill <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/20 18:39:47 by avapaill          #+#    #+#             */
+/*   Updated: 2022/07/20 18:47:16 by avapaill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
